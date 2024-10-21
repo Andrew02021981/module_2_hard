48f8e15f09c17eaa78ecac53e61ddea292748c81
Andrew02021981 <andrejsazonov5487@gmail.com>
2024-10-21 22:35:53 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,14 +1,22 @@
-n = int(input('Введите целое число от 3 до 20: '))
-list_1 = []
+import random
+import time
+def pool_1():
+    list_ = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    n = random.choice(list_)
+    return n
+n = pool_1()
+print('Случайное число от 3 до 20:',n)
+time.sleep(2)
+print('Терпение, мой друг! Еще секундочку)')
+time.sleep(4)
 sum_1 = 0
 str_1 = ''
-for i in range(0, n):
-    list_1.append(i)
 for j in range(1, n):
    for k in range(j, n):
-       sum_1 = list_1[j] + list_1[k]
+       sum_1 = j + k
        if n % sum_1 == 0 and j != k:
             str_1 = str_1 + str(j) + str(k)
 
-print (str_1)
+print ('Ваш пароль:',str_1)
+
 
