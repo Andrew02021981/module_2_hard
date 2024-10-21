63e03e4bae39161fcf4992c8930d46cae47d30cd
Andrew02021981 <andrejsazonov5487@gmail.com>
2024-10-21 16:28:15 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,18 +1,14 @@
 n = int(input('Введите целое число от 3 до 20: '))
 list_1 = []
-list_2 = []
 sum_1 = 0
 str_1 = ''
 for i in range(0, n):
     list_1.append(i)
 for j in range(1, n):
-    for k in range(2, n):
-        sum_1 = list_1[j] + list_1[k]
-        if n % sum_1 == 0 and j != k:
-            #list_2.append(j)
-            #list_2.append(k)
+   for k in range(j, n):
+       sum_1 = list_1[j] + list_1[k]
+       if n % sum_1 == 0 and j != k:
             str_1 = str_1 + str(j) + str(k)
-
 
 print (str_1)
 
